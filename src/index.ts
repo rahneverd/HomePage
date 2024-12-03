bd1f1d65841e52b2f631c0c884c73550aaf6d391
Muhammad Owais <rahneverd@gmail.com>
2024-12-03 17:31:27 +0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({ credentials: true }));
+
+app.use();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
